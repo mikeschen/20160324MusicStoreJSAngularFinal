50423ab9edb01bf2e3095b59c9a36271abc5c728
Michael Schennum and Ryan King <mikeschenphoto@gmail.com>
2016-03-24 08:50:20 -0700

    adds initial files

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,11 +4,26 @@ import { Component } from 'angular2/core';
   selector: 'my-app',
   template: `
   <div class="container">
-    <h1>Skeleton Angular2 App!</h1>
+    <h1>CD Store</h1>
+    <h3 *ngFor="#album of albums">{{ album.name }}</h3>
   </div>
   `
 })
 
-export class AppComponent { 
+export class AppComponent {
+  public albums: Album[];
+  constructor(){
+    this.albums = [
+      new Album("Nevermind", "Nirvana", 9, "rock", 0),
+      new Album("Hey Bo Diddley", "Bo Diddley", 8, "rock", 1),
+      new Album("Enter the Wu-Tang", "Wu-Tang Clan", 7, "rap", 2),
+      new Album("Folsom Prison", "Johnny Cash", 9, "country", 3),
+    ];
+  }
+}
 
+export class Album {
+  constructor(public name: string, public artist: string, public price: number, public genre: string, public id: number){
+
+  }
 }
